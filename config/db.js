d919e610aef3192b5648c7514287b1e9f84e4f27
yjp1406 <yagneshpatelyjp1406@gmail.com>
2023-07-24 13:22:39 +0530

    buf fixed

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   connectionLimit: 10,
   waitForConnections: true,
 });
